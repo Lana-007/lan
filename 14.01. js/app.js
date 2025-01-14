43f42fa0309887a137e9c94738d03237a0eea5f4
Lana-007 <hovhannisyanlana5@gmail.com>
2025-01-14 19:48:57 +0400

    Add files via upload

---
+++ 14.01. js/app.js
@@ -0,0 +1,20 @@
+var xndzor = 300;
+var kg = prompt("qani kg xndzor eq cankanum?");
+var gumar = xndzor * kg;
+alert("duq petq e vjareq"+ gumar+ " dram");
+
+var x = 0.03;
+var qash = prompt("mutqagreq dzer qashy");
+var jur = x * qash;
+alert("dzez anhrajesht e " +jur+ " litr jur");
+
+var dollar = 394;
+var euro = 400;
+var rubli = 3.7;
+var qanak = prompt("mutqagreq gumari chap");
+var dollar1 = qanak / dollar;
+var euro1 = qanak / euro;
+var rubli1 = qanak / rubli;
+alert("-Petq e vcharel aysqan dollar: "+ dollar1 + " USD" +                 
+    "  -Petq e vcharel aysqan euro: " + euro1 + " euro" +
+    "  -Petq e vcharel aysqan rubli: " + rubli1 + " rubli");
